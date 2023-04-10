acb572256a045c6b8b80d2e3dcca8bc470b53231
Vinicius Correia <viniciuscorreia@mac-2.local>
2023-04-10 13:05:24 -0300

    refactor: extracting functions at csv generator methods to improve code readability

--- methods/bill_vote_counts.py
+++ methods/bill_vote_counts.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Tuple
 from methods.csv_utils import write_csv
 from domain.bill import Bill
 from domain.legislator import Legislator
@@ -6,30 +6,49 @@ from domain.vote import Vote
 from domain.vote_result import VoteResult
 from domain.vote_type import VoteType
 
-def generate_bills_csv(legislators: List[Legislator], bills: List[Bill], votes: List[Vote], vote_results: List[VoteResult]) -> None:  
+def get_names_by_legislators_ids(legislators: List[Legislator]) -> Dict[int, str]:
   names_by_legislators_ids: Dict[int, str] = {}
-  vote_results_by_vote_ids: Dict[int, Dict[VoteType, int]] = {}
-  vote_results_by_bill_ids: Dict[int, Dict[VoteType, int]] = {}
-  bills_vote_results = []
-
+  
   for legislator in legislators:
     names_by_legislators_ids[legislator.id] = legislator.name
 
+  return names_by_legislators_ids
+
+def get_vote_results_by_vote_ids(vote_results: List[VoteResult]) -> Dict[int, Dict[VoteType, int]]:
+  vote_results_by_vote_ids: Dict[int, Dict[VoteType, int]] = {}
+  
   for vote_result in vote_results:
     if vote_result.vote_id not in vote_results_by_vote_ids:
       vote_results_by_vote_ids[vote_result.vote_id] = { VoteType.SUPPORTIVE: 0, VoteType.OPPOSITE: 0 }
     vote_results_by_vote_ids[vote_result.vote_id][vote_result.vote_type] += 1
+
+  return vote_results_by_vote_ids
+
+def get_vote_results_by_bill_ids(votes: List[Vote], vote_results_by_vote_ids: Dict[int, Dict[VoteType, int]]) -> Dict[int, Dict[VoteType, int]]:
+  vote_results_by_bill_ids: Dict[int, Dict[VoteType, int]] = {}
 
   for vote in votes:
     if vote.id in vote_results_by_vote_ids:
       vote_results_by_bill_ids[vote.bill_id] = vote_results_by_vote_ids[vote.id]
     else:
       vote_results_by_bill_ids[vote.bill_id] = { VoteType.SUPPORTIVE: 0, VoteType.OPPOSITE: 0 }
+    
+  return vote_results_by_bill_ids
 
+def get_bill_vote_counts(vote_results_by_bill_ids: Dict[int, Dict[VoteType, int]], bill_id: int) -> Tuple[int, int]:
+    supportive_votes = vote_results_by_bill_ids.get(bill_id, {}).get(VoteType.SUPPORTIVE, 0)
+    opposite_votes = vote_results_by_bill_ids.get(bill_id, {}).get(VoteType.OPPOSITE, 0)
+    return supportive_votes, opposite_votes
+
+def get_bills_vote_results(bills: List[Bill], 
+                          names_by_legislators_ids: Dict[int, str], 
+                          vote_results_by_bill_ids: Dict[int, Dict[VoteType, int]]) -> List[Dict[str, str]]:
+                          
+  bills_vote_results = []
+  
   for bill in bills:
-    supported_votes = vote_results_by_bill_ids[bill.id][VoteType.SUPPORTIVE] if bill.id in vote_results_by_bill_ids else 0
-    opposed_votes = vote_results_by_bill_ids[bill.id][VoteType.OPPOSITE] if bill.id in vote_results_by_bill_ids else 0
-    primary_sponsor_name = names_by_legislators_ids[bill.sponsor_id] if bill.sponsor_id in names_by_legislators_ids else "Sponsor not found"
+    supported_votes, opposed_votes = get_bill_vote_counts(vote_results_by_bill_ids, bill.id)
+    primary_sponsor_name = names_by_legislators_ids.get(bill.sponsor_id, "Sponsor not found")
 
     bill_vote_results = {
       "id": bill.id,
@@ -39,5 +58,13 @@ def generate_bills_csv(legislators: List[Legislator], bills: List[Bill], votes: 
       "primary_sponsor": primary_sponsor_name
     }
     bills_vote_results.append(bill_vote_results)
+
+  return bills_vote_results
+
+def generate_bills_csv(legislators: List[Legislator], bills: List[Bill], votes: List[Vote], vote_results: List[VoteResult]) -> None:  
+  names_by_legislators_ids = get_names_by_legislators_ids(legislators)
+  vote_results_by_vote_ids = get_vote_results_by_vote_ids(vote_results)
+  vote_results_by_bill_ids = get_vote_results_by_bill_ids(votes, vote_results_by_vote_ids)
+  bills_vote_results = get_bills_vote_results(bills, names_by_legislators_ids, vote_results_by_bill_ids)
 
   write_csv("bills.csv", bills_vote_results)
--- methods/legislator_vote_counts.py
+++ methods/legislator_vote_counts.py
@@ -1,21 +1,29 @@
-from typing import Dict, List
+from typing import Dict, List, Tuple
 from .csv_utils import write_csv
 from domain.legislator import Legislator
 from domain.vote_result import VoteResult
 from domain.vote_type import VoteType
 
-def generate_legislator_vote_counts_csv(legislators: List[Legislator], vote_results: List[VoteResult]) -> None:
+def get_vote_results_by_legislators_ids(vote_results: List[VoteResult]) -> Dict[int, Dict[VoteType, int]]: 
   vote_results_by_legislators_ids: Dict[int, Dict[VoteType, int]] = {}
-  legislators_vote_results: List[Dict[str, int]] = []
 
   for vote_result in vote_results:
     if vote_result.legislator_id not in vote_results_by_legislators_ids:
       vote_results_by_legislators_ids[vote_result.legislator_id] = { VoteType.SUPPORTIVE: 0, VoteType.OPPOSITE: 0 }
     vote_results_by_legislators_ids[vote_result.legislator_id][vote_result.vote_type] += 1
 
+  return vote_results_by_legislators_ids
+
+def get_legislator_vote_counts(vote_results_by_legislator_ids: Dict[int, Dict[VoteType, int]], legislator_id: int) -> Tuple[int, int]:
+    supportive_votes = vote_results_by_legislator_ids.get(legislator_id, {}).get(VoteType.SUPPORTIVE, 0)
+    opposing_votes = vote_results_by_legislator_ids.get(legislator_id, {}).get(VoteType.OPPOSITE, 0)
+    return supportive_votes, opposing_votes
+
+def get_legislators_vote_results(legislators: List[Legislator], vote_results_by_legislators_ids: Dict[int, Dict[VoteType, int]]) -> List[Dict[str, str]]:
+  legislators_vote_results: List[Dict[str, str]] = []
+
   for legislator in legislators:
-    supported_bills = vote_results_by_legislators_ids[legislator.id][VoteType.SUPPORTIVE] if legislator.id in vote_results_by_legislators_ids else 0
-    opposed_bills = vote_results_by_legislators_ids[legislator.id][VoteType.OPPOSITE] if legislator.id in vote_results_by_legislators_ids else 0
+    supported_bills, opposed_bills = get_legislator_vote_counts(vote_results_by_legislators_ids, legislator.id)
 
     legislator_vote_results = {
       "id": legislator.id,
@@ -24,5 +32,11 @@ def generate_legislator_vote_counts_csv(legislators: List[Legislator], vote_resu
       "num_opposed_bills": opposed_bills
     }
     legislators_vote_results.append(legislator_vote_results)
+  
+  return legislators_vote_results
+
+def generate_legislator_vote_counts_csv(legislators: List[Legislator], vote_results: List[VoteResult]) -> None:
+  vote_results_by_legislators_ids = get_vote_results_by_legislators_ids(vote_results)
+  legislators_vote_results = get_legislators_vote_results(legislators, vote_results_by_legislators_ids)
 
   write_csv("legislators-support-oppose-count.csv", legislators_vote_results)
